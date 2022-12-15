1af47f070084709218cacaf2ed61b7d25951c4b0
SerheyK <korolyonok@inbox.ru>
2022-12-15 23:32:01 +0300

    Добавил решение задач: №41, №43

---
+++ Ex.41/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите первое число");
+string str1 = Console.ReadLine();
+int n1 = int.Parse(str1);
+Console.Write("Введите второе число");
+string str2 = Console.ReadLine();
+int n2 = int.Parse(str2);
+Console.Write("Введите третье число");
+string str3 = Console.ReadLine();
+int n3 = int.Parse(str3);
+Console.Write("Введите четвёртое число");
+string str4 = Console.ReadLine();
+int n4 = int.Parse(str4);
+Console.Write("Введите пятое число");
+string str5 = Console.ReadLine();
+int n5 = int.Parse(str5);
+int[] array = {n1,n2,n3,n4,n5};
+int length = array.Length;
+int result = 0;
+for (int i = 0; i < length; i++)
+{
+    if (array[i] > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine("Количество положительных чисел:" + result);
---
+++ Ex.43/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+Console.Write("Введите первое число ");
+string str1 = Console.ReadLine();
+double b1 = double.Parse(str1);
+Console.Write("Введите второе число ");
+string str2 = Console.ReadLine();
+double k1 = double.Parse(str2);
+Console.Write("Введите третье число ");
+string str3 = Console.ReadLine();
+double b2 = double.Parse(str3);
+Console.Write("Введите четвёртое число ");
+string str4 = Console.ReadLine();
+double k2 = double.Parse(str4);
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
